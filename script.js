e4d5ccfdb3a4ecfa183ab03fe1e0aa780d37ffbe
Ayushman's GitHub <84061842+Ayushman-Jena@users.noreply.github.com>
2026-04-24 18:14:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ elements.forEach(el => observer.observe(el));
 window.addEventListener("load", () => {
     document.querySelector(".hero").style.opacity = "1";
 });
+const slides = document.querySelectorAll('.hero-bg img');
+let current = 0;
+
+setInterval(() => {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+}, 4000); // change every 4 sec
