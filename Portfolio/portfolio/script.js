f99141abfab08715871f6659b0afc84228cb95e5
Ayushman's GitHub <84061842+Ayushman-Jena@users.noreply.github.com>
2026-04-24 14:51:33 +0530

    Create script.js

---
+++ Portfolio/portfolio/script.js
@@ -0,0 +1,15 @@
+const elements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
+
+window.addEventListener("load", () => {
+    document.querySelector(".hero").style.opacity = "1";
+});
